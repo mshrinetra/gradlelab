4b2a5b6794825ffcd20ce037f37efa9721c68cbc
Manvendra Shrinetra <mshrinetra@gmail.com>
2021-04-04 02:49:01 +0530

    Logging added
    log4j 2 added for logging

--- app/src/main/java/com/mshri/gradlelab/App.java
+++ app/src/main/java/com/mshri/gradlelab/App.java
@@ -3,10 +3,21 @@
  */
 package com.mshri.gradlelab;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class App {
+
+    private static final Logger log = LogManager.getLogger(App.class);
+
     public static void main(String[] args) {
         MyUtil.printMsg("First changes in dev branch.");
 
         System.out.println(String.format("Name: %s\nRepo: %s", Config.getProperty("name"), Config.getProperty("repo")));
+
+        log.info("log4j Info message.");
+
+        MyUtil.testError("Testing error logging, no error");
     }
 }
